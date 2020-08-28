88b4422f83128a5c7af70469e29d92d9244651c8
declercq sebastien <sdq@afnor.fr>
2020-08-28 11:14:40 +0200

    GraphQL query to get all Artists ok

--- qwant/music/graphql/query.py
+++ qwant/music/graphql/query.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 from typing import Any
+from django.db.models import Manager
 from graphql.execution.base import ResolveInfo
 import graphene
 from qwant.music.models import Artist
@@ -7,7 +9,13 @@ from .types import ArtistType
 
 class Query(graphene.ObjectType):
     artist: graphene.Field = graphene.Field(ArtistType, slug=graphene.String())
+    artists: graphene.List = graphene.List(ArtistType)
 
     def resolve_artist(self, info: ResolveInfo, **kwargs: Any) -> Artist:
         if slug := kwargs.get('slug'):
             return Artist.objects.get(slug=slug)
+
+    def resolve_artists(
+        self, info: ResolveInfo, **kwargs: Any
+    ) -> Manager[Artist]:
+        return Artist.objects.all()
--- qwant/tests/graphql/test_graphql.py
+++ qwant/tests/graphql/test_graphql.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Dict, Union
+from typing import Any, Callable, Dict, Set, Union
 import json
 from django.http import HttpResponse
 import pytest
@@ -6,7 +6,7 @@ from qwant.music.models import Artist
 from qwant.music.types import APIData
 from qwant.tests import data
 
-
+GraphQLClient = Callable[..., HttpResponse]
 GraphQLResponse = Dict[str, Any]
 
 
@@ -14,7 +14,7 @@ class TestGraphQLModel:
     @pytest.mark.django_db
     @pytest.mark.parametrize('data', data.API_DATA)
     def test_get_one_artist_basic_info(
-        self, data: APIData, graphql_client: Callable[..., HttpResponse]
+        self, data: APIData, graphql_client: GraphQLClient
     ) -> None:
         artist: Artist = Artist.create_from_api_data(**data)
         resp: HttpResponse = graphql_client(
@@ -32,3 +32,25 @@ class TestGraphQLModel:
         assert content['artist']['name'] == artist.name
         assert content['artist']['slug'] == artist.slug
         assert content['artist']['apiId'] == artist.api_id
+
+    @pytest.mark.django_db
+    def test_get_all_artists(self, graphql_client: GraphQLClient) -> None:
+        slugs: Set[str] = set()
+        for artist_data in data.API_DATA:
+            Artist.create_from_api_data(**artist_data)
+            slugs.add(artist_data['slug'])  # type: ignore
+            for similar in artist_data['similar_artists']:  # type: ignore
+                slugs.add(similar['slug'])  # type: ignore
+        resp: HttpResponse = graphql_client(
+            '''
+        query {
+            artists {
+                name
+                slug
+                apiId
+            }
+        }
+        '''
+        )
+        artists: GraphQLResponse = resp.json()['data']['artists']
+        assert {artist['slug'] for artist in artists} == slugs  # type: ignore
